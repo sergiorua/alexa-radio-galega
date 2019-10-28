9e7dbc55ded15823915747f6e907031f265ce4e3
Sergio Rua <sergio@rua.me.uk>
2019-10-28 22:12:39 +0000

    Clean up

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ dockerImage="nikolaik/python-nodejs:python3.7-nodejs10"
 stage=${1:-dev}
 
 [ -x ./lambda/py/node_modules/serverless/bin/serverless ] || ./install.sh
-docker run --rm -e SLS_DEBUG='*' -v $HOME/.aws:/root/.aws -v $(pwd):/data -ti $dockerImage /bin/sh -c "cd /data/lambda/py; npm run deploy-${stage}"
+docker run --rm -e SLS_DEBUG='*' -v $HOME/.aws:/root/.aws -v $(pwd):/data -ti $dockerImage /bin/sh -c "cd /data/lambda/py; npm run deploy-${stage} && ./node_modules/serverless/bin/serverless s3deploy --stage ${stage}"
