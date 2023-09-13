9c9b260219358c056db983ab4d5751b83163061b
EC2 Default User <ec2-user@ip-172-31-31-177.eu-west-1.compute.internal>
2023-09-13 19:35:47 +0000

    upgrade to py 3.10

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-dockerImage="nikolaik/python-nodejs:python3.11-nodejs10"
+dockerImage="nikolaik/python-nodejs:python3.10-nodejs20"
 stage=${1:-dev}
 
 [ -x ./lambda/py/node_modules/serverless/bin/serverless ] || ./install.sh
 
 (cd lambda/py/locales && ./compile.sh)
 
-docker run --rm -e SLS_DEBUG='*' -v $HOME/.aws:/root/.aws -v $(pwd):/data -ti $dockerImage /bin/sh -c "cd /data/lambda/py; npm run deploy-${stage} && ./node_modules/serverless/bin/serverless s3deploy --stage ${stage}"
+docker run --rm -e SLS_DEBUG='*' -v $HOME/.aws:/root/.aws -v $(pwd):/data -ti $dockerImage /bin/sh -c "cd /data/lambda/py; npm run deploy-${stage} && node ./node_modules/serverless/bin/serverless.js s3deploy --stage ${stage}"
